b5fbd59745c9a5fe2239a2e4f7ea3639e27bf538
coderaulia <auliasw89@gmail.com>
2021-08-20 22:47:56 +0700

    prepare for templating

---
+++ resources/views/front/body.blade.php
@@ -0,0 +1,9 @@
+@include('front.parts.header')
+
+<main id="main">
+   
+   @yield('home_content')
+   
+</main>
+
+@include('front.parts.footer')
---
+++ resources/views/home.blade.php
@@ -0,0 +1,5 @@
+@extends('front.body')
+
+@section('home_content')
+
+@endsection
