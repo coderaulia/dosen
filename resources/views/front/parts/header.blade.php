76b1b44937beb476b77948126b6dc520c327f540
coderaulia <auliasw89@gmail.com>
2021-08-28 02:04:43 +0700

    templating

---
+++ resources/views/front/parts/header.blade.php
@@ -0,0 +1,23 @@
+<!-- ======= Header ======= -->
+<header id="header" class="fixed-top d-flex align-items-center">
+<div class="container">
+   <div class="header-container d-flex align-items-center justify-content-between">
+      <div class="logo">
+         <h1 class="text-light"><a href="/"><span>Psikologi Unnes</span></a></h1>
+      <!-- Uncomment below if you prefer to use an image logo -->
+      <!-- <a href="index.html"><img src="assets/img/logo.png" alt="" class="img-fluid"></a>-->
+      </div>
+
+      <nav id="navbar" class="navbar">
+      <ul>
+         <li><a class="nav-link" href="https://psikologi.unnes.ac.id/">Laman Utama</a></li>
+         <li><a class="nav-link" href="#">Dosen</a></li>
+         <li><a class="nav-link" href="#">Contact</a></li>
+         <li><a class="getstarted" href="#">Masuk</a></li>
+      </ul>
+      <i class="bi bi-list mobile-nav-toggle"></i>
+      </nav><!-- .navbar -->
+
+   </div><!-- End Header Container -->
+</div>
+</header><!-- End Header -->
